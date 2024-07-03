b72516c8feb60626370330599d92e573e10d1699
Oscar Daniel Bueno Alvillar <oscar.bueno@adm.urbe.edu>
2024-07-03 11:38:33 -0400

    Modificacion

--- src/app/Chat-Bet/chat-bet.module.ts
+++ src/app/Chat-Bet/chat-bet.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { TranslateModule } from '@ngx-translate/core';
   ],
   imports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    SlickCarouselModule
   ],
   exports: [
     MainPageComponent
--- src/app/Chat-Bet/pages/main-page/main-page.component.ts
+++ src/app/Chat-Bet/pages/main-page/main-page.component.ts
@@ -1,16 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+
+
 
 @Component({
   selector: 'app-chatbet-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
 
+ 
   constructor(private translate: TranslateService) {}
   currentLanguage: string = 'es';
+
+  currentIndex: number = 0;
+  currentIndextwo: number = 0;
+
+ nextParagraph() {
+  if (this.currentIndex < 3) {
+    this.currentIndex++;
+    console.log('Next paragraph:', this.currentIndex);
+  }
+
  
+}
+
+prevParagraph() {
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+    console.log('Previous paragraph:', this.currentIndex);
+  }
+  
+}
+
+nextParagraptwo() {
+  if (this.currentIndextwo < 2) {
+    this.currentIndextwo++;
+    console.log('Next paragraph:', this.currentIndextwo);
+  }
+}
+
+prevParagraphtwo() {
+  if (this.currentIndextwo > 0) {
+    this.currentIndextwo--;
+    console.log('Previous paragraph:', this.currentIndextwo);
+  }
+}
 
   switchLanguage(language: string) {
     this.translate.use(language);
